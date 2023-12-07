7650dbe9ae7d2a43f966d87fbdd1936af841a617
the-unrealist <the-unrealist@users.noreply.github.com>
2023-12-07 08:33:36 +0000

    jekyll build from Action ee3766c7a411c56e9a30077ea05828fa745fcc7f

---
+++ README.md
@@ -0,0 +1,3 @@
+This repo contains the source code for [my blog](https://unrealist.org).
+
+There's nothing to see here. You may be looking for the [Lyra Deep Dive](https://github.com/the-unrealist/lyra-deep-dive).
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Common UI: Switchers and Tabs",
+        "excerpt":"In this article, we’ll learn about the types of switchers in the Common UI plugin and how to leverage them to create tabs. Common UI is a cross-platform UI plugin developed by Epic Games for Unreal Engine 5. There are few talks on YouTube covering the basics of Common UI...","categories": ["Common UI"],
+        "tags": ["unreal","CommonUI","UMG"],
+        "url": "/commonui-switchers-and-tabs/",
+        "teaser": null
+      },{
+        "title": "Common UI: Action Bar",
+        "excerpt":"When the user interface is open in most modern games, you’ll find a list of actions available to the player. This is typically a horizontal bar located at the bottom of the screen. Here are some examples: Destiny 2 Animal Crossing: New Horizons And, of course… Fortnite! The Common UI...","categories": ["Common UI"],
+        "tags": ["unreal","CommonUI","UMG"],
+        "url": "/commonui-actionbar/",
+        "teaser": null
+      },{
+        "title": "A Reference Guide to Custom Blueprint Nodes",
+        "excerpt":"Introduction The Unreal Editor provides a general-purpose graph system that is used by Blueprints, materials, Niagara, and other graph-based features. In this reference guide, we’ll focus on K2Node from which all Blueprint nodes are derived. There’s this fantastic tutorial on creating custom Blueprint nodes. This page is intended as a...","categories": ["Unreal Editor"],
+        "tags": ["unreal","blueprints"],
+        "url": "/custom-blueprint-nodes/",
+        "teaser": null
+      },{
+        "title": "Visualize Actor Components in the Editor with Component Visualizers",
+        "excerpt":"Introduction Working with actor components that don’t have a physical representation may be challenging. Recently, I learned about Component Visualizers which makes it possible to draw anything in the Unreal Editor for each component when selected. In my sandbox construction game, each building piece has a set of polar connection...","categories": ["Unreal Editor"],
+        "tags": ["unreal","components","editor"],
+        "url": "/component-visualizers/",
+        "teaser": null
+      },{
+        "title": "Lyra Deep Dive - Chapter 1: Introduction",
+        "excerpt":"Lyra Deep Dive Series Chapter 1: Introduction Chapter 2: Experiences Chapter 3: Experience Lifecycle Coming soon! Chapter 4: Experience Selection Introduction Lyra is a sample game created by Epic Games to demonstrate Unreal Engine frameworks. This is the first chapter in my Lyra deep dive. I am walking through the...","categories": ["Lyra"],
+        "tags": ["unreal","Lyra"],
+        "url": "/lyra-part-1/",
+        "teaser": null
+      },{
+        "title": "Lyra Deep Dive - Chapter 2: Experiences",
+        "excerpt":"This is the second chapter in the Lyra Deep Dive series. Lyra introduces the concept of “experiences” which essentially are modular game modes. In this chapter, we’ll walk through various data assets which define a Lyra Experience. Lyra Deep Dive Series Chapter 1: Introduction Chapter 2: Experiences Chapter 3: Experience...","categories": ["Lyra"],
+        "tags": ["unreal","Lyra"],
+        "url": "/lyra-part-2/",
+        "teaser": null
+      },{
+        "title": "Lyra Deep Dive - Chapter 3: Experience Lifecycle",
+        "excerpt":"This is the third chapter in the Lyra Deep Dive series. In the previous chapter, we’ve learned about how experiences are defined. In this chapter, we’ll take a deep dive into the lifecycle of an experience. Lyra Deep Dive Series Chapter 1: Introduction Chapter 2: Experiences Chapter 3: Experience Lifecycle...","categories": ["Lyra"],
+        "tags": ["unreal","Lyra"],
+        "url": "/lyra-part-3/",
+        "teaser": null
+      }]
